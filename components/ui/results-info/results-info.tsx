20898bd79c38379f504b845c198289964eb8d048
Pierre Garamendi <pierre.garamendi@gmail.com>
2023-07-24 23:45:56 -0500

    this is a test

--- components/ui/results-info/results-info.tsx
+++ components/ui/results-info/results-info.tsx
@@ -5,8 +5,8 @@ const ResultsInfo = () => {
     const search = searchParams.get('search')
     return (
         <div className='flex flex-row items-center justify-center'>
-            <span className="text-lg font-semibold text-gray-500 dark:text-white">Resultados para&nbsp;
-                <span className="text-lg font-semibold text-blue-700">{search}&nbsp;</span>
+            <span className="text-lg font-semibold text-gray-500 dark:text-white">Resultados para
+                <span className="text-lg font-semibold text-blue-700">&nbsp;{search}&nbsp;</span>
                 <span className='text-sm font-semibold text-gray-500 dark:text-gray-400'>(1000+)</span>
             </span>
         </div>
