dc4fafc3c3e80b3738856c07330bccdd2198431a
Pierre Garamendi <pierre.garamendi@gmail.com>
2023-08-09 12:18:31 -0500

    error fixed

--- constants/pagination/pagination.constants.ts
+++ constants/pagination/pagination.constants.ts
@@ -1,11 +1,18 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
+"use client"
+
+import { useSearchParams } from "next/navigation"
 
 export const getQueriesSearch = () => {
-    const searchParams = useSearchParams()
-    const search = searchParams.get('search')
-    const sort = searchParams.get('sort') || 'mp'
-    const page = searchParams.get('page')
-    const limit = searchParams.get('limit')
-    return { search, sort, page, limit }
+  const searchParams = useSearchParams()
+  let search = "search"
+  let sort = "mp"
+  let page = "1"
+  let limit = ""
+  if (searchParams !== null) {
+    search = searchParams.get("search") || ""
+    sort = searchParams.get("sort") || "mp"
+    page = searchParams.get("page") || "1"
+    limit = searchParams.get("limit") || ""
+  }
+  return { search, sort, page, limit }
 }
